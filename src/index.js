6408b34a95406dc7c092090863b22e9d18c6eb3c
Alexandr Sanatar <allexvik@gmail.com>
2022-12-12 20:42:37 +0200

    upd base not full

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+var debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
+let searchValue = '';
+
+const inputEl = document.querySelector('input#search-box');
+const countryEl = document.querySelector('ul.country-list');
+
+inputEl.addEventListener('input', debounce(inputValue, 300));
+
+function inputValue(e) {
+  console.log('e.target.value: ', e.target.value);
+  searchValue = e.target.value;
+  console.log('searchValue: ', searchValue);
+  getCountries(searchValue);
+}
+
+function getCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(r => r.json())
+    .then(renderCountriesList)
+    .catch(() => {
+      console.log('nothing found');
+      return;
+    });
+}
+
+function renderCountriesList(countries) {
+  const markup = countries
+    .map(oneCountry => {
+      return `<li>
+            <p><b>Name</b>: ${oneCountry.name.common}</p>
+          </li>`;
+    })
+    .join('');
+  countryEl.innerHTML = markup;
+}
